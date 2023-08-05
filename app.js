96441d48e429540198e2c02bbc32ebb7f52599d2
datlearner <112308111+datlearner@users.noreply.github.com>
2023-08-05 13:41:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+myFun = () =>{
+ 
+
+document.getElementById('white').style.backgroundColor = 'white' 
+ 
+
+}
+myFunn = () =>{
+ 
+
+document.getElementById('whitee').style.backgroundColor = 'white' 
+ 
+
+}
+myFunnn = () =>{
+ 
+
+document.getElementById('whiteee').style.backgroundColor = 'white' 
+ 
+
+}
+myFunnnn = () =>{
+ 
+
+document.getElementById('whiteeee').style.backgroundColor = 'white' 
+ 
+
+}
